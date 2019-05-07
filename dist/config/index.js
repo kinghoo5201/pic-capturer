1632c53e7ccd94f41a947d7208185ba062621319
wb-hjh418165 <wb-hjh418165@alibaba-inc.com>
2019-05-07 19:55:48 +0800

    auto commit by shell

--- dist/config/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const isDev = process.argv[2] === "develop";
-const port = 1234;
-exports.default = {
-    port,
-    uploadDir: "/upload/ups/",
-    isDev,
-};
