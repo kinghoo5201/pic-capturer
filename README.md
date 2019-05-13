e647095fa2f718d2d2a060752e42db976f11afde
kinghoo_h <970489393@qq.com>
2019-05-14 00:12:22 +0800

    auto commit by shell

--- README.md
+++ README.md
@@ -3,11 +3,3 @@
 站点图片抓取对比工具
 
 [中文 api](https://zhaoqize.github.io/puppeteer-api-zh_CN/)
-
-运行方式
-
-打开两个终端：
-
-`npm run watch`
-
-`npm start`
--- server/utils/Capturer.ts
+++ server/utils/Capturer.ts
@@ -1,63 +1,74 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 export default class Capturer {
-    /**实例 */
-    private browser: puppeteer.Browser;
-    /**page实例 */
-    private page: puppeteer.Page;
-    /**配置 */
-    private launchOpt?: puppeteer.LaunchOptions;
+  /**实例 */
+  private browser: puppeteer.Browser;
+  /**page实例 */
+  private page: puppeteer.Page;
+  /**配置 */
+  private launchOpt?: puppeteer.LaunchOptions;
 
-    public constructor(options?: puppeteer.LaunchOptions) {
-        if (options) {
-            this.initOpt(options);
-        }
-        this.initBrowser()
-            .then(() => this.initPage());
+  public constructor(options?: puppeteer.LaunchOptions) {
+    if (options) {
+      this.initOpt(options);
     }
-    /**截图 */
-    public async screenShoot(url: string, pageOptions?: puppeteer.PageFnOptions, screenShootOpt?: puppeteer.ScreenshotOptions) {
-        if (!this.page) {
-            return false;
-        }
-        await this.page.goto(url, pageOptions ? pageOptions : {});
-        await this.page.screenshot(screenShootOpt ? screenShootOpt : {})
-        return true;
+    this.initBrowser().then(() => this.initPage());
+  }
+  /**截图 */
+  public async screenShoot(
+    url: string,
+    screenShootOpt: puppeteer.ScreenshotOptions,
+    pageCfg: { [propsName: string]: any } = {}
+  ) {
+    if (!this.page) {
+      return false;
     }
-    /**更新配置 */
-    public async updateOptions(launchOpt: puppeteer.LaunchOptions) {
-        this.close();
-        this.initOpt(launchOpt);
-        await this.initBrowser();
-        await this.initPage();
-        return true;
+    await this.page.goto(url, {
+      waitUntil: "load",
+      ...pageCfg
+    });
+    await this.page.screenshot(screenShootOpt);
+    return true;
+  }
+  /**更新配置 */
+  public async updateOptions(launchOpt: puppeteer.LaunchOptions) {
+    this.close();
+    this.initOpt(launchOpt);
+    await this.initBrowser();
+    await this.initPage();
+    return true;
+  }
+  /**关闭browser */
+  public close() {
+    if (!this.browser) {
+      return;
     }
-    /**关闭browser */
-    public close() {
-        if (!this.browser) {
-            return;
-        }
-        this.browser.close();
-    }
-    /**初始化browser */
-    private async initBrowser() {
-        this.browser = await puppeteer.launch(this.launchOpt ? {
+    this.browser.close();
+  }
+  /**初始化browser */
+  private async initBrowser() {
+    this.browser = await puppeteer.launch(
+      this.launchOpt
+        ? {
             ...this.launchOpt
-        } : {});
-        console.log('browser is running!');
-        return true;
+          }
+        : {}
+    );
+    console.log("browser is running!");
+    return true;
+  }
+  /**初始化页面 */
+  private async initPage() {
+    if (!this.browser) {
+      return false;
     }
-    /**初始化页面 */
-    private async initPage() {
-        if (!this.browser) {
-            return false;
-        }
-        this.page = await this.browser.newPage();
-        console.log('page is opening!');
-        return true;
-    }
-    /**初始化配置 */
-    private initOpt(options: puppeteer.LaunchOptions) {
-        this.launchOpt = options;
-    }
+    this.page = await this.browser.newPage();
+    this.page.setCacheEnabled(false);
+    console.log("page is opening!");
+    return true;
+  }
+  /**初始化配置 */
+  private initOpt(options: puppeteer.LaunchOptions) {
+    this.launchOpt = options;
+  }
 }
