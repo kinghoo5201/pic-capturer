87e4fc076d027c06998c67c21ed20e6c375da216
wb-hjh418165 <wb-hjh418165@alibaba-inc.com>
2019-05-13 19:27:28 +0800

    auto commit by shell

--- server/routers/index.ts
+++ server/routers/index.ts
@@ -1,9 +1,22 @@
 import { Express } from "express";
+import Capturer from '../utils/Capturer';
 
+const capturer = new Capturer();
+
+// pptr是否正在运行
+(global as any).isRunPptr = false;
 function router(app: Express) {
-  app.use('/',function(req,res,next){
-    return res.render('index');
-  });
+    app.get('/', function(req, res, next) {
+        return res.render('index');
+    });
+    app.get('/api-status', function(req, res, next) {
+        return res.json({
+            isRunning: (global as any).isRunPptr,
+        });
+    });
+    app.post('/api-capture', function(req, res, next) {
+        console.log(req.fields);
+    });
 }
 
 export default router;
